2ed161f62e3abd31a5381a9e732b290035d7919a
EVLT <evlt@cbs.nl>
2024-03-26 18:32:46 +0100

    update with pyscaffold

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "tabular2xls"
-copyright = "2022, EVLT"
+copyright = "2024, EVLT"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
